7fd6cd2d8c3615349bb0ed776b8e62deebf2296e
Tiago Hudler <tiagohudler@gmail.com>
2024-09-08 23:33:16 -0300

    - Add search by phrase functionality - Add delete and edit button icons

--- front/script.js
+++ front/script.js
@@ -111,16 +111,32 @@ function createTaskList(tasks) {
         editButton.textContent = "Edit";
         editButton.classList.add("edit-button");
         editButton.addEventListener("click", () => editTask(task.id, tasks));
+        editButton.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+                <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>
+            </svg>
+        `;
 
         // Create Exclude Button
         const deleteButton = document.createElement("button");
         deleteButton.textContent = "Delete";
         deleteButton.classList.add("delete-button");
         deleteButton.addEventListener("click", () => deleteTask(task.id, tasks));
+        deleteButton.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+            </svg>
+        `;
+
+        // Create a button container div
+        const buttonContainer = document.createElement("div");
+        buttonContainer.classList.add("button-container");
+        buttonContainer.appendChild(editButton);
+        buttonContainer.appendChild(deleteButton);
 
         // Append buttons to task div
-        taskDiv.appendChild(editButton);
-        taskDiv.appendChild(deleteButton);
+        taskDiv.appendChild(buttonContainer);
 
         taskDiv.id = task.id;
 
@@ -153,3 +169,12 @@ function closeFormById(id) {
 document.getElementById("cancelUpdateButton").addEventListener("click", () => closeFormById("cancelUpdateButton"));
 
 document.getElementById("cancelCreateButton").addEventListener("click", () => closeFormById("cancelCreateButton"));
+
+// search by phrase
+
+document.getElementById("searchByWord").addEventListener("click", async () => {
+    let searchPhrase = document.getElementById("searchButton").value;
+    let tasks = await getAllTasks();
+    let filteredTasks = tasks.filter((task) => task.name.includes(searchPhrase) || task.description.includes(searchPhrase));
+    createTaskList(filteredTasks);
+});
